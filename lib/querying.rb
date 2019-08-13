1f5a14d0d6315fbd71f5bb76aac7ba39468590ca
Nicole Dow <nikki.dow71@gmail.com>
2019-08-13 22:08:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,7 +22,10 @@ ORDER BY COUNT(species) DESC LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+FROM authors
+LEFT JOIN series ON authors.id = series.author_id
+LEFT JOIN subgenres ON series.subgenre_id = subgenres.id;"
 end
 
 def select_series_title_with_most_human_characters
